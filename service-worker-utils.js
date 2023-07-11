e6e288326d838366667294ff83cb1d2b75ef8d01
Abdul-Mueed-Shahbaz <bscs18056@itu.edu.pk>
2023-07-12 03:26:03 +0500

    add overlay to detect clicks

--- service-worker-utils.js
+++ service-worker-utils.js
@@ -1,100 +1,135 @@
-if (window.self === window.top) {
-  const parentWindowCoords = {
-    rootWindowHeight: window.innerHeight,
-    rootWindowWidth: window.innerWidth,
-  };
+// Create overlay element
+let overlay = document.createElement("div");
+overlay.id = "overlay";
+document.body.appendChild(overlay);
 
-  chrome.storage.local.set({
-    parentWindowCoords: JSON.stringify(parentWindowCoords),
-  });
-} else {
-  const windowHeight = window.innerHeight;
-  const windowWidth = window.innerWidth;
+// Apply CSS styles to the overlay
+overlay.style.position = "fixed";
+overlay.style.top = "0";
+overlay.style.left = "0";
+overlay.style.width = "100%";
+overlay.style.height = "100%";
+overlay.style.zIndex = "9999";
+overlay.style.background = "rgba(0, 0, 0, 0.01)"; // Set a nearly transparent background
 
-  if (windowHeight && windowWidth) {
-    chrome.storage.local.get(["parentWindowCoords"]).then((result) => {
-      const parentWindowCoords = JSON.parse(result.parentWindowCoords);
+const trackClicks = (event) => {
+  let x = event.clientX;
+  let y = event.clientY;
 
-      const heightRatio = parentWindowCoords.rootWindowHeight / windowHeight;
-      const widthRatio = parentWindowCoords.rootWindowWidth / windowWidth;
-
-      const mappedHeight = +(windowHeight * heightRatio);
-      const mappedWidth = +(windowWidth * widthRatio);
-
-      console.log(window.parent);
-
-      chrome.storage.local.set({
-        coordinatesRatio: JSON.stringify({
-          heightRatio,
-          widthRatio,
-        }),
-      });
-    });
-  }
-}
-const clickTracker = async (event) => {
-  const storeTrackingInfo = (result, clickData) => {
-    let jsonTrackingData = null;
-    if (result.trackingData) {
-      const trackingData = JSON.parse(result.trackingData);
-      trackingData.push(clickData);
-      jsonTrackingData = JSON.stringify(trackingData);
-      chrome.storage.local.set({ trackingData: jsonTrackingData });
-      console.log(window.self === window.top ? "root" : "iframe");
-      console.log(trackingData);
-      alert(jsonTrackingData);
-      return;
-    }
-    jsonTrackingData = JSON.stringify([clickData]);
-    chrome.storage.local.set({ trackingData: jsonTrackingData });
-    console.log(window.self === window.top ? "root" : "iframe");
-    console.log([clickData]);
-    alert(jsonTrackingData);
-  };
-  const getCurrentTime = () => {
-    let currentTime = new Date();
-    let currentHour = currentTime.getHours();
-    let currentMinute = currentTime.getMinutes();
-    return { currentHour, currentMinute };
-  };
-  const getWindowToScreenCoordinates = async (event) => {
-    let screenX = event.screenX;
-    let screenY = event.screenY;
-
-    let windowX = event.clientX;
-    let windowY = event.clientY;
-
-    if (window.self === window.top) {
-      return { screenX, screenY, windowX, windowY };
-    } else {
-      // const result = await chrome.storage.local.get(["coordinatesRatio"]);
-      // const coordinatesRatio = JSON.parse(result.coordinatesRatio);
-      // const mappedHeight = Math.trunc(windowY * coordinatesRatio.heightRatio);
-      // const mappedWidth = Math.trunc(windowX * coordinatesRatio.widthRatio);
-      return { screenX, screenY, windowX: screenX, windowY: screenY };
-    }
-
-    return { screenX, screenY, windowX, windowY };
-  };
-
-  const { windowX, windowY } = await getWindowToScreenCoordinates(event);
-  let { currentHour, currentMinute } = getCurrentTime();
-  currentMinute =
-    currentMinute < 10 ? (currentMinute = "0" + currentMinute) : currentMinute;
-
-  const clickData = {
-    type: event.type,
-    Coordinates: {
-      x: windowX,
-      y: windowY,
-    },
-    time: currentHour + ":" + currentMinute,
-  };
-
-  chrome.storage.local
-    .get(["trackingData"])
-    .then((res) => storeTrackingInfo(res, clickData));
+  // Do something with the coordinates
+  console.log("X: " + x + ", Y: " + y);
 };
 
-document.addEventListener("click", clickTracker);
-document.addEventListener("contextmenu", clickTracker);
+// Add event listeners to the overlay
+overlay.addEventListener("click", trackClicks);
+overlay.addEventListener("contextmenu", trackClicks);
+
+// Set the overlay element to be transparent to pointer events
+overlay.style.pointerEvents = "none";
+
+// Enable pointer events on child elements of the overlay
+const overlayChildren = overlay.querySelectorAll("*");
+for (let i = 0; i < overlayChildren.length; i++) {
+  overlayChildren[i].style.pointerEvents = "auto";
+}
+
+// if (window.self === window.top) {
+//   const parentWindowCoords = {
+//     rootWindowHeight: window.innerHeight,
+//     rootWindowWidth: window.innerWidth,
+//   };
+
+//   chrome.storage.local.set({
+//     parentWindowCoords: JSON.stringify(parentWindowCoords),
+//   });
+// } else {
+//   const windowHeight = window.innerHeight;
+//   const windowWidth = window.innerWidth;
+
+//   if (windowHeight && windowWidth) {
+//     chrome.storage.local.get(["parentWindowCoords"]).then((result) => {
+//       const parentWindowCoords = JSON.parse(result.parentWindowCoords);
+
+//       const heightRatio = parentWindowCoords.rootWindowHeight / windowHeight;
+//       const widthRatio = parentWindowCoords.rootWindowWidth / windowWidth;
+
+//       const mappedHeight = +(windowHeight * heightRatio);
+//       const mappedWidth = +(windowWidth * widthRatio);
+
+//       console.log(window.parent);
+
+//       chrome.storage.local.set({
+//         coordinatesRatio: JSON.stringify({
+//           heightRatio,
+//           widthRatio,
+//         }),
+//       });
+//     });
+//   }
+// }
+// const clickTracker = async (event) => {
+//   const storeTrackingInfo = (result, clickData) => {
+//     let jsonTrackingData = null;
+//     if (result.trackingData) {
+//       const trackingData = JSON.parse(result.trackingData);
+//       trackingData.push(clickData);
+//       jsonTrackingData = JSON.stringify(trackingData);
+//       chrome.storage.local.set({ trackingData: jsonTrackingData });
+//       console.log(window.self === window.top ? "root" : "iframe");
+//       console.log(trackingData);
+//       alert(jsonTrackingData);
+//       return;
+//     }
+//     jsonTrackingData = JSON.stringify([clickData]);
+//     chrome.storage.local.set({ trackingData: jsonTrackingData });
+//     console.log(window.self === window.top ? "root" : "iframe");
+//     console.log([clickData]);
+//     alert(jsonTrackingData);
+//   };
+//   const getCurrentTime = () => {
+//     let currentTime = new Date();
+//     let currentHour = currentTime.getHours();
+//     let currentMinute = currentTime.getMinutes();
+//     return { currentHour, currentMinute };
+//   };
+//   const getWindowToScreenCoordinates = async (event) => {
+//     let screenX = event.screenX;
+//     let screenY = event.screenY;
+
+//     let windowX = event.clientX;
+//     let windowY = event.clientY;
+
+//     if (window.self === window.top) {
+//       return { screenX, screenY, windowX, windowY };
+//     } else {
+//       // const result = await chrome.storage.local.get(["coordinatesRatio"]);
+//       // const coordinatesRatio = JSON.parse(result.coordinatesRatio);
+//       // const mappedHeight = Math.trunc(windowY * coordinatesRatio.heightRatio);
+//       // const mappedWidth = Math.trunc(windowX * coordinatesRatio.widthRatio);
+//       return { screenX, screenY, windowX: screenX, windowY: screenY };
+//     }
+
+//     return { screenX, screenY, windowX, windowY };
+//   };
+
+//   const { windowX, windowY } = await getWindowToScreenCoordinates(event);
+//   let { currentHour, currentMinute } = getCurrentTime();
+//   currentMinute =
+//     currentMinute < 10 ? (currentMinute = "0" + currentMinute) : currentMinute;
+
+//   const clickData = {
+//     type: event.type,
+//     Coordinates: {
+//       x: windowX,
+//       y: windowY,
+//     },
+//     time: currentHour + ":" + currentMinute,
+//   };
+
+//   chrome.storage.local
+//     .get(["trackingData"])
+//     .then((res) => storeTrackingInfo(res, clickData));
+// };
+
+// document.addEventListener("click", clickTracker);
+// document.addEventListener("contextmenu", clickTracker);
--- service-worker.js
+++ service-worker.js
@@ -5,30 +5,30 @@
 console.log(
   "This prints to the console of the service worker (background script)"
 );
-chrome.scripting.registerContentScripts([
-  {
-    id: "tracker",
-    matches: ["<all_urls>", "http://*/*", "https://*/*"],
-    allFrames: true,
-    js: ["service-worker-utils.js"],
-  },
-]);
+// chrome.scripting.registerContentScripts([
+//   {
+//     id: "tracker",
+//     matches: ["<all_urls>", "http://*/*", "https://*/*"],
+//     allFrames: true,
+//     js: ["service-worker-utils.js"],
+//   },
+// ]);
 
-// Importing and using functionality from external files is also possible.
-// importScripts("service-worker-utils.js");
+// // Importing and using functionality from external files is also possible.
+// // importScripts("service-worker-utils.js");
 
-// If you want to import a file that is deeper in the file hierarchy of your
-// extension, simply do `importScripts('path/to/file.js')`.
-// The path should be relative to the file `manifest.json`.
+// // If you want to import a file that is deeper in the file hierarchy of your
+// // extension, simply do `importScripts('path/to/file.js')`.
+// // The path should be relative to the file `manifest.json`.
 
-chrome.action.onClicked.addListener(() => {
-  chrome.tabs.query({ active: true, currentWindow: true }).then(([tab]) => {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      files: ["service-worker-utils.js"],
-    });
-  });
-});
+// chrome.action.onClicked.addListener(() => {
+//   chrome.tabs.query({ active: true, currentWindow: true }).then(([tab]) => {
+//     chrome.scripting.executeScript({
+//       target: { tabId: tab.id },
+//       files: ["service-worker-utils.js"],
+//     });
+//   });
+// });
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.status == "complete") {
