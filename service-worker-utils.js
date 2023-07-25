f8925ec33d0dd1105d796edc91d257f2f4afb715
Abdul-Mueed-Shahbaz <bscs18056@itu.edu.pk>
2023-07-26 00:22:13 +0500

    poc complete

--- service-worker-utils.js
+++ service-worker-utils.js
@@ -1,5 +1,6 @@
 if (typeof clickTracker !== "function") {
   let mousedown = false;
+
   // Create a new MutationObserver
   var observer = new MutationObserver(function (mutationsList) {
     for (var i = 0; i < mutationsList.length; i++) {
@@ -34,54 +35,52 @@ if (typeof clickTracker !== "function") {
 
   observer.observe(document, { childList: true, subtree: true });
 
-  const clickTracker = async (event) => {
-    const storeTrackingInfo = (result, clickData) => {
-      let jsonTrackingData = null;
+  const storeTrackingInfo = (result, clickData) => {
+    let jsonTrackingData = null;
 
-      if (result.trackingData) {
-        const trackingData = JSON.parse(result.trackingData);
-        trackingData.push(clickData);
-        jsonTrackingData = JSON.stringify(trackingData);
-        chrome.storage.local
-          .set({ trackingData: jsonTrackingData })
-          .then(() => {
-            const resetTimeOut = setTimeout(() => {
-              clickTriggered = false;
-              mouseDownTriggered = false;
-              clearTimeout(resetTimeOut);
-            }, 100);
-          });
-        console.log(trackingData);
-        // alert(jsonTrackingData);
-        return;
-      }
-
-      jsonTrackingData = JSON.stringify([clickData]);
+    if (result.trackingData) {
+      const trackingData = JSON.parse(result.trackingData);
+      trackingData.push(clickData);
+      jsonTrackingData = JSON.stringify(trackingData);
       chrome.storage.local.set({ trackingData: jsonTrackingData }).then(() => {
-        clickTriggered = false;
-        mouseDownTriggered = false;
+        const resetTimeOut = setTimeout(() => {
+          clickTriggered = false;
+          mouseDownTriggered = false;
+          clearTimeout(resetTimeOut);
+        }, 100);
       });
-      console.log([clickData]);
+      console.log(trackingData);
       // alert(jsonTrackingData);
-    };
+      return;
+    }
 
-    const getCurrentTime = () => {
-      let currentTime = new Date();
-      let currentHour = currentTime.getHours();
-      let currentMinute = currentTime.getMinutes();
-      return { currentHour, currentMinute };
-    };
+    jsonTrackingData = JSON.stringify([clickData]);
+    chrome.storage.local.set({ trackingData: jsonTrackingData }).then(() => {
+      clickTriggered = false;
+      mouseDownTriggered = false;
+    });
+    console.log([clickData]);
+    // alert(jsonTrackingData);
+  };
 
-    const getWindowToScreenCoordinates = async (event) => {
-      let screenX = event.screenX;
-      let screenY = event.screenY;
+  const getCurrentTime = () => {
+    let currentTime = new Date();
+    let currentHour = currentTime.getHours();
+    let currentMinute = currentTime.getMinutes();
+    return { currentHour, currentMinute };
+  };
 
-      let windowX = event.clientX;
-      let windowY = event.clientY;
+  const getWindowToScreenCoordinates = async (event) => {
+    let screenX = event.screenX;
+    let screenY = event.screenY;
 
-      return { screenX, screenY, windowX, windowY };
-    };
+    let windowX = event.clientX;
+    let windowY = event.clientY;
 
+    return { screenX, screenY, windowX, windowY };
+  };
+
+  const clickTracker = async (event) => {
     const { windowX, windowY } = await getWindowToScreenCoordinates(event);
 
     let { currentHour, currentMinute } = getCurrentTime();
@@ -149,4 +148,21 @@ if (typeof clickTracker !== "function") {
       clickTracker(event);
     }
   });
+  window.onload = function () {
+    let { currentHour, currentMinute } = getCurrentTime();
+    currentMinute =
+      currentMinute < 10
+        ? (currentMinute = "0" + currentMinute)
+        : currentMinute;
+
+    const clickData = {
+      type: event.type,
+      time: currentHour + ":" + currentMinute,
+      url: window.location.href,
+    };
+
+    chrome.storage?.local
+      .get(["trackingData"])
+      .then((res) => storeTrackingInfo(res, clickData));
+  };
 }
