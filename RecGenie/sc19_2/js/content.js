8a3194e3d889375da986e43e07f0f4fe5140196c
Abdul-Mueed-Shahbaz <bscs18056@itu.edu.pk>
2023-08-02 03:04:25 +0500

    stop interaction while loading

--- RecGenie/sc19_2/js/content.js
+++ RecGenie/sc19_2/js/content.js
@@ -1,4 +1,204 @@
 if (typeof clickTracker !== "function") {
+  function startTracking() {
+    let mousedown = false;
+    // Create a new MutationObserver
+    var observer = new MutationObserver(function (mutationsList) {
+      for (var i = 0; i < mutationsList.length; i++) {
+        var mutation = mutationsList[i];
+
+        // Check if a node was added to the DOM
+        if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
+          // Loop through the added nodes
+          for (var j = 0; j < mutation.addedNodes.length; j++) {
+            var addedNode = mutation.addedNodes[j];
+            // Check if the added node is an iframe
+            const iframes = document.querySelectorAll("iframe");
+
+            iframes.forEach((iframe) => {
+              const iframeRect = iframe.getBoundingClientRect();
+              const iframeX = iframeRect.left;
+              const iframeY = iframeRect.top;
+
+              if ((iframeX > 0 || iframeY > 0) && chrome.storage?.local) {
+                chrome.storage.local.set({
+                  iframePosition: JSON.stringify({
+                    iframeX: parseInt(iframeX),
+                    iframeY: parseInt(iframeY),
+                  }),
+                });
+              }
+            });
+          }
+        }
+      }
+    });
+
+    observer.observe(document, { childList: true, subtree: true });
+
+    const storeTrackingInfo = (result, clickData) => {
+      let jsonTrackingData = null;
+
+      if (result.trackingData) {
+        const trackingData = JSON.parse(result.trackingData);
+        trackingData.push(clickData);
+        jsonTrackingData = JSON.stringify(trackingData);
+        chrome.storage.local
+          .set({ trackingData: jsonTrackingData })
+          .then(() => {
+            const resetTimeOut = setTimeout(() => {
+              clickTriggered = false;
+              mouseDownTriggered = false;
+              clearTimeout(resetTimeOut);
+            }, 100);
+          });
+        console.log(trackingData);
+        // alert(jsonTrackingData);
+        return;
+      }
+
+      jsonTrackingData = JSON.stringify([clickData]);
+      chrome.storage.local.set({ trackingData: jsonTrackingData }).then(() => {
+        clickTriggered = false;
+        mouseDownTriggered = false;
+      });
+      console.log([clickData]);
+      // alert(jsonTrackingData);
+    };
+
+    const getCurrentTime = async () => {
+      const res = await chrome.storage.local.get(["recordStartTime"]);
+      const recordStartTimeString = res.recordStartTime;
+      const storedDatetime = new Date(recordStartTimeString);
+      let currentTime = new Date();
+
+      const timeDifferenceMillis =
+        currentTime.getTime() - storedDatetime.getTime();
+
+      const secondsTillDatetime = timeDifferenceMillis / 1000;
+
+      return secondsTillDatetime.toFixed(2);
+    };
+    const getWindowToScreenCoordinates = async (event) => {
+      let screenX = event.screenX;
+      let screenY = event.screenY;
+
+      let windowX = event.clientX;
+      let windowY = event.clientY;
+
+      return { screenX, screenY, windowX, windowY };
+    };
+    // Define the debounce function
+    function debounce(func, delay) {
+      let timer;
+      return function (...args) {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+          func.apply(this, args);
+        }, delay);
+      };
+    }
+
+    // Your original scroll event handler
+    const handleScroll = (event) => {
+      const scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+      const scrollLeft =
+        window.pageXOffset || document.documentElement.scrollLeft;
+      chrome.storage?.local.get(["trackingData"]).then((res) => {
+        getCurrentTime().then((time) => {
+          const clickData = {
+            type: event.type,
+            time,
+            url: window.location.href,
+            scrollCoordinates: {
+              top: scrollTop,
+              left: scrollLeft,
+            },
+          };
+          storeTrackingInfo(res, clickData);
+        });
+      });
+    };
+
+    // Debounce the scroll event handler to be called after 500 milliseconds of scroll inactivity
+    const debouncedScrollHandler = debounce(handleScroll, 500);
+
+    const clickTracker = async (event) => {
+      const { windowX, windowY } = await getWindowToScreenCoordinates(event);
+      const time = await getCurrentTime();
+      const clickData = {
+        type: event.type,
+        Coordinates: {
+          x: windowX,
+          y: windowY,
+        },
+        time,
+        url: window.location.href,
+      };
+
+      if (window.top !== window.self) {
+        try {
+          const res = await chrome.storage.local.get(["iframePosition"]);
+          if (res.iframePosition) {
+            const windowWidth = window.innerWidth;
+            const windowHeight = window.innerHeight;
+            clickData.iframePosition = {
+              ...JSON.parse(res.iframePosition),
+              iframeWidth: windowWidth,
+              iframeHeight: windowHeight,
+            };
+          } else {
+            const iframes = document.querySelectorAll("iframe");
+            iframes.forEach((iframe) => {
+              const iframeRect = iframe.getBoundingClientRect();
+              const iframeX = iframeRect.left;
+              const iframeY = iframeRect.top;
+
+              chrome.storage.local.set({
+                iframePosition: JSON.stringify({
+                  iframeX,
+                  iframeY,
+                }),
+              });
+            });
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      chrome.storage?.local
+        .get(["trackingData"])
+        .then((res) => storeTrackingInfo(res, clickData))
+        .catch((err) => console.log(err));
+    };
+    document.addEventListener("mousedown", (event) => {
+      mousedown = true;
+      clickTracker(event);
+      const cTimeout = setTimeout(() => {
+        mousedown = false;
+        clearTimeout(cTimeout);
+      }, 100);
+    });
+    document.addEventListener("click", (event) => {
+      if (!mousedown) {
+        clickTracker(event);
+      }
+    });
+    window.onload = async function (event) {
+      chrome.storage?.local.get(["trackingData"]).then((res) => {
+        getCurrentTime().then((time) => {
+          const clickData = {
+            type: event.type,
+            time,
+            url: window.location.href,
+          };
+          storeTrackingInfo(res, clickData);
+        });
+      });
+    };
+    window.addEventListener("scroll", debouncedScrollHandler);
+  }
   chrome.storage.local.get(null, (result) => {
     if (!result.rec) {
       let e = document.createElement("img");
@@ -43,204 +243,11 @@ if (typeof clickTracker !== "function") {
               (o -= 1))
             : ((document.getElementById("loaderDiv2").style.display = "none"),
               (document.getElementById("ss-ext-timer").style.display = "none"),
+              startTracking(),
               clearInterval(n));
         }, 1e3);
-    }
-  });
-  let mousedown = false;
-
-  // Create a new MutationObserver
-  var observer = new MutationObserver(function (mutationsList) {
-    for (var i = 0; i < mutationsList.length; i++) {
-      var mutation = mutationsList[i];
-
-      // Check if a node was added to the DOM
-      if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
-        // Loop through the added nodes
-        for (var j = 0; j < mutation.addedNodes.length; j++) {
-          var addedNode = mutation.addedNodes[j];
-          // Check if the added node is an iframe
-          const iframes = document.querySelectorAll("iframe");
-
-          iframes.forEach((iframe) => {
-            const iframeRect = iframe.getBoundingClientRect();
-            const iframeX = iframeRect.left;
-            const iframeY = iframeRect.top;
-
-            if ((iframeX > 0 || iframeY > 0) && chrome.storage?.local) {
-              chrome.storage.local.set({
-                iframePosition: JSON.stringify({
-                  iframeX: parseInt(iframeX),
-                  iframeY: parseInt(iframeY),
-                }),
-              });
-            }
-          });
-        }
-      }
-    }
-  });
-
-  observer.observe(document, { childList: true, subtree: true });
-
-  const storeTrackingInfo = (result, clickData) => {
-    let jsonTrackingData = null;
-
-    if (result.trackingData) {
-      const trackingData = JSON.parse(result.trackingData);
-      trackingData.push(clickData);
-      jsonTrackingData = JSON.stringify(trackingData);
-      chrome.storage.local.set({ trackingData: jsonTrackingData }).then(() => {
-        const resetTimeOut = setTimeout(() => {
-          clickTriggered = false;
-          mouseDownTriggered = false;
-          clearTimeout(resetTimeOut);
-        }, 100);
-      });
-      console.log(trackingData);
-      // alert(jsonTrackingData);
       return;
     }
-
-    jsonTrackingData = JSON.stringify([clickData]);
-    chrome.storage.local.set({ trackingData: jsonTrackingData }).then(() => {
-      clickTriggered = false;
-      mouseDownTriggered = false;
-    });
-    console.log([clickData]);
-    // alert(jsonTrackingData);
-  };
-
-  const getCurrentTime = async () => {
-    const res = await chrome.storage.local.get(["recordStartTime"]);
-    const recordStartTimeString = res.recordStartTime;
-    const storedDatetime = new Date(recordStartTimeString);
-    let currentTime = new Date();
-
-    const timeDifferenceMillis =
-      currentTime.getTime() - storedDatetime.getTime();
-
-    const secondsTillDatetime = timeDifferenceMillis / 1000;
-
-    return secondsTillDatetime.toFixed(2);
-  };
-  const getWindowToScreenCoordinates = async (event) => {
-    let screenX = event.screenX;
-    let screenY = event.screenY;
-
-    let windowX = event.clientX;
-    let windowY = event.clientY;
-
-    return { screenX, screenY, windowX, windowY };
-  };
-  // Define the debounce function
-  function debounce(func, delay) {
-    let timer;
-    return function (...args) {
-      clearTimeout(timer);
-      timer = setTimeout(() => {
-        func.apply(this, args);
-      }, delay);
-    };
-  }
-
-  // Your original scroll event handler
-  const handleScroll = (event) => {
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    const scrollLeft =
-      window.pageXOffset || document.documentElement.scrollLeft;
-    chrome.storage?.local.get(["trackingData"]).then((res) => {
-      getCurrentTime().then((time) => {
-        const clickData = {
-          type: event.type,
-          time,
-          url: window.location.href,
-          scrollCoordinates: {
-            top: scrollTop,
-            left: scrollLeft,
-          },
-        };
-        storeTrackingInfo(res, clickData);
-      });
-    });
-  };
-
-  // Debounce the scroll event handler to be called after 500 milliseconds of scroll inactivity
-  const debouncedScrollHandler = debounce(handleScroll, 500);
-
-  const clickTracker = async (event) => {
-    const { windowX, windowY } = await getWindowToScreenCoordinates(event);
-    const time = await getCurrentTime();
-    const clickData = {
-      type: event.type,
-      Coordinates: {
-        x: windowX,
-        y: windowY,
-      },
-      time,
-      url: window.location.href,
-    };
-
-    if (window.top !== window.self) {
-      try {
-        const res = await chrome.storage.local.get(["iframePosition"]);
-        if (res.iframePosition) {
-          const windowWidth = window.innerWidth;
-          const windowHeight = window.innerHeight;
-          clickData.iframePosition = {
-            ...JSON.parse(res.iframePosition),
-            iframeWidth: windowWidth,
-            iframeHeight: windowHeight,
-          };
-        } else {
-          const iframes = document.querySelectorAll("iframe");
-          iframes.forEach((iframe) => {
-            const iframeRect = iframe.getBoundingClientRect();
-            const iframeX = iframeRect.left;
-            const iframeY = iframeRect.top;
-
-            chrome.storage.local.set({
-              iframePosition: JSON.stringify({
-                iframeX,
-                iframeY,
-              }),
-            });
-          });
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    chrome.storage?.local
-      .get(["trackingData"])
-      .then((res) => storeTrackingInfo(res, clickData))
-      .catch((err) => console.log(err));
-  };
-  document.addEventListener("mousedown", (event) => {
-    mousedown = true;
-    clickTracker(event);
-    const cTimeout = setTimeout(() => {
-      mousedown = false;
-      clearTimeout(cTimeout);
-    }, 100);
+    startTracking();
   });
-  document.addEventListener("click", (event) => {
-    if (!mousedown) {
-      clickTracker(event);
-    }
-  });
-  window.onload = async function (event) {
-    chrome.storage?.local.get(["trackingData"]).then((res) => {
-      getCurrentTime().then((time) => {
-        const clickData = {
-          type: event.type,
-          time,
-          url: window.location.href,
-        };
-        storeTrackingInfo(res, clickData);
-      });
-    });
-  };
-  window.addEventListener("scroll", debouncedScrollHandler);
 }
